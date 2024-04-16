1b65b420419f16f9a24dfc56db03df915ea72f10
mrinalcs <mrinalvbu@gmail.com>
2024-04-16 15:37:09 +0530

    1

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,47 +1,8 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
 
 export default function Footer() {
   return (
     <footer>
-      <ul>
-        <li>
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p>rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/vercel/next.js"
-          >
-            <ArrowIcon />
-            <p>github</p>
-          </a>
-        </li>
-        
-      </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+      <p>
         © {new Date().getFullYear()} MIT Licensed
       </p>
     </footer>
