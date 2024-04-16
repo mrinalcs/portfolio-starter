ff3a63524d22430b6f5d3d7c86ffd1cecde0af54
mrinalcs <mrinalvbu@gmail.com>
2024-04-16 15:22:46 +0530

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
@@ -44,14 +42,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      >
+      <body>
+        <main>
           <Navbar />
           {children}
           <Footer />
